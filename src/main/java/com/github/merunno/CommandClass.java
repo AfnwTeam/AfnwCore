c14815ee690906acef98e081e0fccb2a7bda490a
merunno <merunno92@gmail.com>
2021-09-27 11:48:23 +0900

    :sparkles: アイテムを受け取った際に音がなるように

--- src/main/java/com/github/merunno/CommandClass.java
+++ src/main/java/com/github/merunno/CommandClass.java
@@ -4,6 +4,7 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
@@ -21,11 +22,13 @@ public class CommandClass implements CommandExecutor {
         if(command.getName().equalsIgnoreCase("randomitem")) {
             String id = args[0];
             var player = Bukkit.getPlayer(id);
+            Location loc = (Objects.requireNonNull(player)).getLocation();
             List<Material> materials = Arrays.asList(Material.values());
             Collections.shuffle(materials);
             ItemStack itemStack = new ItemStack(materials.get(0), 8);
             Objects.requireNonNull(player).getInventory().addItem(itemStack);
             player.sendMessage(ChatColor.YELLOW + pluginAfnw + "アイテムの配布に成功しました。");
+            sound(loc);
         } else if (command.getName().equalsIgnoreCase("vote-link")) {
             sender.sendMessage(ChatColor.GREEN + "=== Vote Site ===");
             sender.sendMessage(Vote + "Japan Minecraft Servers");
@@ -39,5 +42,9 @@ public class CommandClass implements CommandExecutor {
     private static boolean isAllowed(Material type) {
         if(!type.isItem()) return false;
         return type != Material.BEDROCK;
+    }
+
+    private void sound(Location loc) {
+        Objects.requireNonNull(loc.getWorld()).playSound(loc, Sound.BLOCK_AMETHYST_CLUSTER_BREAK, 1, 1);
     }
 }
