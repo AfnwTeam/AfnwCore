7939f102c93fc0ac82f2b325cf0bdce5bfbab0f4
Sakuma Sho <merunno92@gmail.com>
2021-10-10 15:50:17 +0900

    Release :: AfnwCore v3.0.0 (#10)
    * :bookmark: Change: Version
    
    * :truck: Moved: クラス分け
    
    * :fire: Code: カラーの削除
    
    * :sparkles: Add: インベントリがいっぱいだったときにインベントリを閉じるたびに警告を出すように
    
    * :recycle: refactoring: nullチェックを加える
    
    * :ambulance: Fix: 送信メッセージの一部調整を行いました。
    
    * :bug: Fix: インベントリがいっぱいじゃないのに警告文が表示される問題の修正
    
    * :memo: パッチノートの追加

---
+++ src/main/java/com/github/merunno/listener/InventoryClose.java
@@ -0,0 +1,20 @@
+package com.github.merunno.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+
+public class InventoryClose implements Listener {
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent closeEvent) {
+        Player player = (Player) closeEvent.getPlayer();
+        int emptySlot = player.getInventory().firstEmpty();
+        if (emptySlot == -1) {
+            player.sendMessage(ChatColor.RED + "[AfnwCore] インベントリがいっぱいです。");
+        }
+    }
+}
