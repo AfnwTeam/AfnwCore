64afd526b4637b2584b9d9d434c86cafc535ed55
merunno <merunno92@gmail.com>
2021-11-22 01:05:11 +0900

    [Add] コマンドの追加

---
+++ src/main/java/com/github/merunno/commands/hoten.java
@@ -0,0 +1,76 @@
+package com.github.merunno.commands;
+
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class hoten implements CommandExecutor {
+
+    String pluginAfnw = "[AfnwCore] ";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (command.getName().equalsIgnoreCase("hoten")) {
+            String id = args[0];
+            var player = Bukkit.getPlayer(id);
+            if (player == null) {
+                return true;
+            }
+            Inventory player_inventory = player.getInventory();
+            Location loc = player.getLocation();
+            int emptySlot = player_inventory.firstEmpty();
+            if (emptySlot == -1) {
+                player.sendMessage(ChatColor.RED + pluginAfnw + "運営による補填に失敗しました。");
+                player.sendMessage(ChatColor.RED + "インベントリがいっぱいで配布できませんでした。再度運営に連絡してください。");
+                inventory_sound(loc);
+                return true;
+            }
+            List<Material> materials = Arrays.asList(Material.values());
+            Collections.shuffle(materials);
+            ItemStack itemStack = new ItemStack(materials.get(0), 8);
+            Objects.requireNonNull(player).getInventory().addItem(itemStack);
+            player.sendMessage(ChatColor.YELLOW + pluginAfnw + "補填に成功しました。ご迷惑おかけしました。");
+            sound(loc);
+            particle(loc);
+            if(loc.getWorld() != null) {
+                return true;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isAllowed(Material type) {
+        if (!type.isItem()) return false;
+        switch (type) {
+            case BEDROCK:
+            case STRUCTURE_BLOCK:
+            case STRUCTURE_VOID:
+            case COMMAND_BLOCK:
+            case CHAIN_COMMAND_BLOCK:
+            case COMMAND_BLOCK_MINECART:
+            case REPEATING_COMMAND_BLOCK: return false;
+            default: return true;
+        }
+    }
+
+    private void sound(Location loc) {
+        Objects.requireNonNull(loc.getWorld()).playSound(loc, Sound.BLOCK_AMETHYST_CLUSTER_BREAK, 1, 1);
+    }
+
+    private void particle(Location loc) {
+        Objects.requireNonNull(loc.getWorld()).playEffect(loc, Effect.DRAGON_BREATH, 0, 14);
+    }
+
+
+    public void inventory_sound(Location loc) {
+        Objects.requireNonNull(loc.getWorld()).playSound(loc, Sound.BLOCK_ANVIL_DESTROY, 1, 1);
+    }
+}
