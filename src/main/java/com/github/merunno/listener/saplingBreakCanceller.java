c2125eba727ffeb0a7c3ed2752664c4451889df1
merunno <merunno92@gmail.com>
2021-11-22 19:36:09 +0900

    [Change] OPは破壊出来るように

--- src/main/java/com/github/merunno/listener/saplingBreakCanceller.java
+++ src/main/java/com/github/merunno/listener/saplingBreakCanceller.java
@@ -1,38 +1,25 @@
 package com.github.merunno.listener;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class saplingBreakCanceller implements Listener {
+
+    String prefix = "[AfnwCore] ";
 
     @EventHandler
     public void onBreakSapling(BlockBreakEvent breakEvent) {
-        Item item = (Item) breakEvent.getBlock();
         Player player = breakEvent.getPlayer();
-        List<Material> materials = Arrays.asList(Material.values());
-        if(item.hasMetadata(String.valueOf(materials))) {
+        if(breakEvent.getBlock().getType().name().contains("SAPLING")) {
+            if(player.hasPermission("afnw.op.commands")) {
+                player.sendMessage(ChatColor.YELLOW + prefix + "オペレーター権限で苗木を破壊しました。");
+                return;
+            }
             breakEvent.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "[AfnwCore] " + "一度設置した苗木は破壊できません。");
-        }
-    }
-
-    private static boolean isAllowed(Material type) {
-        switch (type) {
-            case OAK_SAPLING:
-            case SPRUCE_SAPLING:
-            case BIRCH_SAPLING:
-            case JUNGLE_SAPLING:
-            case ACACIA_SAPLING:
-            case DARK_OAK_SAPLING: return false;
-            default: return true;
+            player.sendMessage(ChatColor.RED + "[AfnwCore] " + "一度設置した苗木は破壊できません。破壊の申請はサーバー内にいる運営に連絡してください。");
         }
     }
 }
