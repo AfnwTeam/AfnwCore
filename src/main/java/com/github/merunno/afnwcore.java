676fcba3159c6db698993dd407434bdceb9b97a8
merunno <merunno92@gmail.com>
2021-11-29 16:50:51 +0900

    [Add] 苗木が植えられている土を破壊できないように

--- src/main/java/com/github/merunno/afnwcore.java
+++ src/main/java/com/github/merunno/afnwcore.java
@@ -31,6 +31,7 @@ public final class afnwcore extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new InventoryClose(), this);
         getServer().getPluginManager().registerEvents(new saplingBreakCanceller(), this);
         getServer().getPluginManager().registerEvents(new brew(), this);
+        getServer().getPluginManager().registerEvents(new chat(), this);
         getLogger().info("イベントの読み込み.... Done.");
 
         getLogger().info("Afnwの読み込みが完了しました。");
