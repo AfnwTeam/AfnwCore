72bffee7190d279dbcd21fa735fe27db02924a1c
5hak3 <shake.salmo@gmail.com>
2021-12-06 10:52:03 +0900

    [Fix] Stop Water Flowing to Crops and Saplings

--- src/main/java/com/github/merunno/listener/CropsBreakCanceller.java
+++ src/main/java/com/github/merunno/listener/CropsBreakCanceller.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 
 public class CropsBreakCanceller implements Listener {
 
@@ -16,7 +17,7 @@ public class CropsBreakCanceller implements Listener {
         Player player = event.getPlayer();
         Block target = event.getBlock();
         if (Tag.CROPS.isTagged(target.getLocation().add(0,1,0).getBlock().getType())){
-            player.sendMessage(ChatColor.RED + "[AfnwCore] 一度植えた成長しきっていない農作物の下にあるブロックは破壊できません。");
+            player.sendMessage(ChatColor.RED + "[AfnwCore] 農作物が植えられている耕地は破壊できません。");
             event.setCancelled(true);
             return;
         }
@@ -28,5 +29,21 @@ public class CropsBreakCanceller implements Listener {
         player.sendMessage(ChatColor.RED + "[AfnwCore] 一度植えた成長しきっていない農作物は破壊できません。");
         event.setCancelled(true);
     }
+
+    /**
+     * 農作物に対して水流が流れ込むのを阻害する
+     */
+    @EventHandler
+    public void onFlowing(BlockFromToEvent event){
+        Block target = event.getToBlock();
+        if(!(Tag.CROPS.isTagged(target.getType()))) return;
+        if (target.getBlockData() instanceof Ageable){
+            Ageable crop = (Ageable)target.getBlockData();
+            if (crop.getMaximumAge() == crop.getAge()) return;
+        }
+        event.setCancelled(true);
+    }
+
+
 
 }
--- src/main/java/com/github/merunno/listener/saplingBreakCanceller.java
+++ src/main/java/com/github/merunno/listener/saplingBreakCanceller.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 
 public class saplingBreakCanceller implements Listener {
 
@@ -24,7 +25,17 @@ public class saplingBreakCanceller implements Listener {
             player.sendMessage(ChatColor.RED + "[AfnwCore] " + "一度設置した苗木は破壊できません。破壊の申請はサーバー内にいる運営に連絡してください。");
         } else if(Tag.SAPLINGS.isTagged(breakEvent.getBlock().getLocation().add(0, 1, 0).getBlock().getType())) {
             breakEvent.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "[AfnwCore] " + "一度設置した苗木が植えられている土は破壊できません。苗木が育ち切るのを待つか、苗木の破壊を運営に申請してください。");
+            player.sendMessage(ChatColor.RED + "[AfnwCore] " + "苗木が植えられている土は破壊できません。苗木が育ち切るのを待つか、苗木の破壊を運営に申請してください。");
+        }
+    }
+
+    /**
+     * 苗木に対して水流が流れ込むのを阻害する
+     */
+    @EventHandler
+    public void onFlowing(BlockFromToEvent event){
+        if (Tag.SAPLINGS.isTagged(event.getToBlock().getType()) && !(event.getToBlock().getType() == Material.FLOWERING_AZALEA || event.getToBlock().getType() == Material.FLOWERING_AZALEA_LEAVES)) {
+            event.setCancelled(true);
         }
     }
 }
